edcc0646159c7483af137d53201c0d05891fbf5c
JasonRodriguez1474 <91709253+JasonRodriguez1474@users.noreply.github.com>
2022-05-15 16:31:25 -0700

    defined clone to copy

--- main.tf
+++ main.tf
@@ -12,3 +12,18 @@ provider "proxmox" {
  pm_api_token_id = "${proxmox_api_token_id}"
  pm_api_token_secret = "${proxmox_api_secret}"
 }
+
+resource "proxmox_vm_qemu" "iac_server" {
+  name        = "iac_server_${count.index + 1}"
+  target_node = "kankuro"
+  clone       = "OL8-Template"
+  full_clone = "true"
+
+  ### or for a Clone VM operation
+  # clone = "template to clone"
+
+  ### or for a PXE boot VM operation
+  # pxe = true
+  # boot = "net0;scsi0"
+  # agent = 0
+}
