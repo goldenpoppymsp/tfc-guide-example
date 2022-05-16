53512229665c07afeb5f4e7691b596981322a2cc
JasonRodriguez1474 <91709253+JasonRodriguez1474@users.noreply.github.com>
2022-05-15 17:34:05 -0700

    Terraform 0.11 and earlier required type constraints to be given in quotes, but that form is now deprecated and will be removed in a future version of Terraform. Remove the quotes around the word string.

--- variables.tf
+++ variables.tf
@@ -1,9 +1,9 @@
 variable "proxmox_api_secret" {
   description = "API Token secret for proxmox"
-  type     = "string"
+  type = string
 }
 
 variable "proxmox_api_token_id" {
   description = "API Token ID for proxmox"
-  type     = "string"
+  type = string
 }
