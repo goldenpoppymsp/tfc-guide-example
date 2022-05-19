313144e160f6d06e2a2001d65bb3acf45ebfcf8a
JasonRodriguez1474 <91709253+JasonRodriguez1474@users.noreply.github.com>
2022-05-18 22:52:10 -0700

    hard code test

--- variables.tf
+++ variables.tf
@@ -1,9 +1,11 @@
 variable "proxmox_api_secret" {
   description = "API Token secret for proxmox"
   type = string
+  default = "70cbd7f4-1802-4211-b0a2-0d44a28b6c8c"
 }
 
 variable "proxmox_api_token_id" {
   description = "API Token ID for proxmox"
   type = string
+  default = "terraform@authentik!terraformcloud"
 }
